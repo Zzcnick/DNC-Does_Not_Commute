5df18881e62946f2cf8dac2a6af355dc8adcf4fe
Zzcnick <zzhen@stuy.edu>
2016-12-02 14:49:17 -0500

    add skeletons for utils, move bootstrap code

---
+++ utils/darksky.py
@@ -0,0 +1,8 @@
+import json, urllib2
+
+# API Key: AIzaSyCnkOEu9Xyc2O3sNqzmTzCNub6e_kSoUeY
+
+# ================================================
+#                Google API Calls
+# ================================================
+
