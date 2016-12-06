0404d0ff5fdfaf4cf560e097012a17bee2ceea16
Zzcnick <zzhen@stuy.edu>
2016-12-06 14:40:41 -0500

    add more geocoding functions

--- utils/google.py
+++ utils/google.py
@@ -3,8 +3,10 @@ import json, urllib2, requests
 # API Key: AIzaSyCnkOEu9Xyc2O3sNqzmTzCNub6e_kSoUeY
 
 # ================================================
-#                Google API Calls
+#               Google API Helpers
 # ================================================
+def webstring(adr):
+    return "+".join(adr.split())
 
 # ================================================
 #                   Geolocation
@@ -32,21 +34,33 @@ def call_gc_ll(lat, lng):
     gc_dict = json.loads(gc_get)
     return gc_dict
 
+def call_gc_ad(adr):
+    add = webstring(adr)
+    gc_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyCnkOEu9Xyc2O3sNqzmTzCNub6e_kSoUeY"%(add)
+    gc_data = urllib2.urlopen(gc_url)
+    gc_get = gc_data.read()
+    gc_dict = json.loads(gc_get)
+    return gc_dict
+    
 def gc_address(lat, lng):
     gc_dict = call_gc_ll(lat, lng)
     return gc_dict['results'][0]['formatted_address']
 
-# gl = gl_location()
-# print gc_address(gl[0], gl[1])
+def gc_latlng(adr):
+    gc_dict = call_gc_ad(adr)
+    return [gc_dict['results'][0]['geometry']['location']['lat'],
+            gc_dict['results'][0]['geometry']['location']['lng']]
+
+print gc_latlng("345 Chambers Street, NY 10282")
 
 # ================================================
 #                Distance Matrix
 # ================================================
 def call_dm(lat1, lng1, lat2, lng2):
     add1_raw = gc_address(lat1,lng1)
-    add1 = "+".join(add1_raw.split())
+    add1 = webstring(add1_raw)
     add2_raw = gc_address(lat2,lng2)
-    add2 = "+".join(add2_raw.split())
+    add2 = webstring(add2_raw)
     dm_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=%s&destinations=%s,NY&key=AIzaSyCnkOEu9Xyc2O3sNqzmTzCNub6e_kSoUeY"%(add1,add2)
     dm_data = urllib2.urlopen(dm_url)
     dm_get = dm_data.read()
@@ -65,7 +79,6 @@ def dm_dist(lat1, lng1, lat2, lng2):
 # d2 = ["40.7925920","-73.8465270"]
 # print "Estimated Time: ", dm_eta(d1[0],d1[1],d2[0],d2[1])
 # print "Distance: ", dm_dist(d1[0],d1[1],d2[0],d2[1])
-
 
 # ================================================
 #                Directions API
