2df8e6d007014285f928db15c2bad6bedca797ba
Brian-Lu <brianlu22@yahoo.com>
2016-12-14 00:01:12 -0500

    Added lateness function

--- utils/google.py
+++ utils/google.py
@@ -1,4 +1,5 @@
-import json, urllib, urllib2
+import json, urllib, urllib2, datetime
+
 
 key_gen = open("keys.txt", "r").read().strip().split("\n")[0]
 key_emb = open("keys.txt", "r").read().strip().split("\n")[1]
@@ -191,6 +192,24 @@ def get_directions_walking(origin, destination):
     print line_counter
     return retStr
 
+def time_difference(dm_dict, arrival_time):
+    departure_time = get_ETA(dm_dict)
+    print departure_time
+    indexOfArrival = arrival_time.find(":")
+    indexOfDeparture = departure_time.find(":")
+    hour_arrival = int(arrival_time[0:indexOfArrival])
+    hour_departure = int(departure_time[0:indexOfDeparture])
+    if hour_arrival == 12 and arrival_time[-2] == 'a':
+        hour_arrival = 0
+    elif arrival_time[-2]== 'p' and arrival_time[0:2] != 12:
+        hour_arrival += 12
+    if hour_departure == 12 and departure_time[-2] == 'a':
+        hour_departure = 0
+    elif departure_time[-2] == 'p':
+        hour_departure += 12
+    arrival_minutes = (hour_arrival * 60) + int(arrival_time[indexOfArrival + 1: indexOfArrival + 3])
+    departure_minutes = (hour_departure * 60) + int(departure_time[indexOfDeparture + 1: indexOfDeparture + 3])
+    return departure_minutes - arrival_minutes
 
 
 # UNNECESSARY
@@ -205,3 +224,5 @@ def get_directions_walking(origin, destination):
 #    return retStr
 
 # print get_directions_transit("345 Chambers Street", "67-41 Burns Street")
+#dm_dict = get_directions_dict("Little Neck", "Penn Station", "transit")
+#print time_difference(dm_dict, '1:44AM')
